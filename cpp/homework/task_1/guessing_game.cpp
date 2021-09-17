0b46f190bdcecf82dfad8ec91fe52476e7dcf0ce
jwdotpark <jwdotpark@me.com>
2021-09-17 15:00:20 +0200

    hw2 done

--- cpp/homework/task_1/guessing_game.cpp
+++ cpp/homework/task_1/guessing_game.cpp
@@ -1,35 +1,37 @@
 // guessing game
 #include <iostream>
-#include <string>
+// #include <string>
 using namespace std;
 
-int random_number = rand() % 100;
-// get a random number from 1 to 100
-
 int main()
+
 {
+  srand(time(NULL));
+  int random_number = rand() % 100;
+  // get a random number from 1 to 100
+
   int user_input;
+
   cout << "Guess a number between 1 and 100" << endl;
   cin >> user_input;
 
-  while (user_input != random_number)
+  while (true)
   {
     if (user_input > random_number)
     {
       cout << "Too high, guess again" << endl;
       cin >> user_input;
     }
-    else
+    else if (user_input < random_number)
     {
       cout << "Too low, guess again" << endl;
       cin >> user_input;
     }
-
-    if (user_input == random_number)
+    else if (user_input == random_number)
     {
       cout << "You guess the right number, " << user_input;
+      break;
     }
   }
-
-  return 0;
+  // return 0;
 }
