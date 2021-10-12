34dfb83fc5a44f9a39ffe4b0916aba0f9828b852
Jongwoo Park <jwdotpark@me.com>
2021-10-12 12:46:59 +0200

    added temp/ultrasonic/lcd

--- physical_interface/test.ino
+++ physical_interface/test.ino
@@ -1,21 +1,18 @@
 int red = 0;
 int blue = 0;
 
+// dht sensor
+#include "dht.h"
+#define dht_apin A5  // Analog Pin sensor is connected to
+dht DHT;
+
+// ultrasonic sensor
 #define echoPin 4  // attach pin D2 Arduino to pin Echo of HC-SR04
 #define trigPin 5  // attach pin D3 Arduino to pin Trig of HC-SR04
 
 // defines variables
 long duration;  // variable for the duration of sound wave travel
 int distance;   // variable for the distance measurement
-//
-// // matrix led
-// #include <LedControl.h>
-//
-// const int dataInPin = 9;
-// const int clockPin = 10;
-// const int csPin = 11;
-// LedControl lc = LedControl(dataInPin, clockPin, csPin, 1);
-//
 
 // lcd
 #include <LiquidCrystal.h>
@@ -38,41 +35,24 @@ void setup() {
   pinMode(echoPin, INPUT);   // Sets the echoPin as an INPUT
   Serial.begin(57600);       // // Serial Communication is starting with 9600 of
                              // baudrate speed
-  Serial.println(
-      "Ultrasonic Sensor HC-SR04 Test");  // print some text in Serial Monitor
-  Serial.println("with Arduino UNO R3");
-
-  //  // matrix led
-  //   while (!Serial) { }
-  //  lc.shutdown(0,false);
-  //  lc.setIntensity(0,8);
-  //  lc.clearDisplay(0);
+  Serial.print("board init");
 
   // lcd
   lcd.begin(16, 2);
 }
 
-// matrix counter init
-// int counter = 0;
-
 void loop() {
-  // led + switch
+  // dht11
+  DHT.read11(dht_apin);
+  Serial.print("Current humidity = ");
+  Serial.print(DHT.humidity);
+  Serial.print("%  ");
+  Serial.print("temperature = ");
+  Serial.print(DHT.temperature);
+  Serial.println("C  ");
+
   red = digitalRead(2);
   blue = digitalRead(3);
-
-  if (red == HIGH) {
-    digitalWrite(12, HIGH);
-    Serial.print("red led on\n");
-  } else {
-    digitalWrite(12, LOW);
-  }
-
-  if (blue == HIGH) {
-    digitalWrite(13, HIGH);
-    Serial.print("blue led on\n");
-  } else {
-    digitalWrite(13, LOW);
-  }
 
   // SR04 ultra sonic range sensor
   // Clears the trigPin condition
@@ -96,21 +76,28 @@ void loop() {
   Serial.print(distance);
   Serial.println(" cm");
 
-  //  // matrix led
-  //    lc.clearDisplay(0);
-  //  // Explain why this is a bad thing: flicker. We should buffer
-  //  // and load deltas. NOTE: not needed once we add the for-loop
-  //  //  Device is mounted sideways: discuss orientation
-  //  lc.setColumn(0, 4, counter);
-  //  counter = (counter + 1) % 256;
-  //  delay(100);
+  if (distance < 100) {
+    // green if close
+    digitalWrite(13, HIGH);
+    digitalWrite(12, LOW);
+    Serial.print("red led on\n");
+  } else {
+    // else blue
+    digitalWrite(13, LOW);
+    digitalWrite(12, HIGH);
+    Serial.print("blue led on\n");
+  }
 
   // lcd
   lcd.clear();
-  lcd.print("   cm far away!");
-  delay(10);
-  lcd.setCursor(0, 1);
   lcd.print(distance);
+  lcd.print(" cm far away!");
+  lcd.setCursor(0, 1);
+  lcd.print(DHT.humidity);
+  lcd.print("% / ");
+  lcd.print(DHT.temperature);
+  lcd.print("C");
+  lcd.setCursor(1, 1);
   delay(500);
   lcd.clear();
 }
