214909eac7f856492ef3580487583c9ad44008d6
Jongwoo Park <jongwoo.park@code.berlin>
2021-11-02 22:11:46 +0100

    wip: ds&a file organizing

---
+++ practical_ds_a/sort/bubble-sort.js
@@ -0,0 +1,16 @@
+const array = require("../utils/array");
+const swap = require("../utils/swap");
+
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      // console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(array));
