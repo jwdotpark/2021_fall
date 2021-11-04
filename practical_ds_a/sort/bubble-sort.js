2a8b523d3630de58b80bec20d8a88e3f89e95ede
Jongwoo Park <jongwoo.park@code.berlin>
2021-11-04 16:55:54 +0100

    wip: ds&a bubble sort description changed

--- practical_ds_a/sort/bubble-sort.js
+++ practical_ds_a/sort/bubble-sort.js
@@ -1,4 +1,4 @@
-const array = Array.from({length: 100000}, () => Math.floor(Math.random() * 100000));
+const array = Array.from({ length: 100000 }, () => Math.floor(Math.random() * 100000));
 
 const swap = (arr, index1, index2) => {
   [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
@@ -18,3 +18,23 @@ function bubbleSort(arr) {
 
 // 22s
 console.log(bubbleSort(array));
+
+// slightly better optimized bubble sort
+function bubbleSort(arr) {
+  // remove unnecessary loop
+  let noSwap = true;
+  // loop over backwards 
+  for (let i = 0; i < arr.length; i++) {
+    // inner loop runs fewer times as outer loop runs through
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1);
+        noSwap = false;
+      }
+      if (noSwap) {
+        break;
+      }
+    }
+  }
+  return arr;
+}
